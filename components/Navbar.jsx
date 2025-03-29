39102c8e1f394ee08303e6388ce99e24dac6c81c
elyse502 <elyseniyibizi502@gmail.com>
2025-03-29 07:30:03 +0200

    Integrate Clerk Auth in the project

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,11 @@ import { assets } from "@/assets/assets";
 import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
+import { useClerk, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  const { isSeller, router } = useAppContext();
+  const { isSeller, router, user } = useAppContext();
+  const { openSignIn } = useClerk();
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
@@ -42,10 +44,19 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {user ? (
+          <>
+            <UserButton />
+          </>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
@@ -57,7 +68,10 @@ const Navbar = () => {
             Seller Dashboard
           </button>
         )}
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 hover:text-gray-900 transition"
+        >
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
