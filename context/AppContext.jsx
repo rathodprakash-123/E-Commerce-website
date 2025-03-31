0770dc2d7b2ec3e39f18eb53767d701c996fb650
elyse502 <elyseniyibizi502@gmail.com>
2025-03-31 08:56:04 +0200

    Integrate API to fetch User Data with the Front-end part

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -15,6 +17,7 @@ export const AppContextProvider = (props) => {
   const router = useRouter();
 
   const { user } = useUser();
+  const { getToken } = useAuth();
 
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
@@ -30,10 +33,21 @@ export const AppContextProvider = (props) => {
       if (user.publicMetadata.role === "seller") {
         setIsSeller(true);
       }
-      setUserData(userDummyData);
+
+      const token = await getToken();
+
+      const { data } = await axios.get("/api/user/data", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserData(data.user);
+        setCartItems(data.user.cartItems);
+      } else {
+        toast.error(data.message);
+      }
     } catch (error) {
-      console.error("Error fetching user data:", error);
-      setUserData(false);
+      toast.error(error.message);
     }
   };
 
@@ -90,6 +104,7 @@ export const AppContextProvider = (props) => {
 
   const value = {
     user,
+    getToken,
     currency,
     router,
     isSeller,
