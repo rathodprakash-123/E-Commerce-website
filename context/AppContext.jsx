967e5c8cd568f4ff3fb58def4d571f47a5d54802
elyse502 <elyseniyibizi502@gmail.com>
2025-04-01 10:29:01 +0200

    Integrate API to fetch User's Cart Data & API to update Cart Data with the Frontend

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -69,7 +69,22 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = 1;
     }
     setCartItems(cartData);
-    toast.success("Item added to cart");
+
+    if (user) {
+      try {
+        const token = await getToken();
+
+        await axios.post(
+          "/api/cart/update",
+          { cartData },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+
+        toast.success("Item added to cart");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
   };
 
   const updateCartQuantity = async (itemId, quantity) => {
@@ -80,6 +95,22 @@ export const AppContextProvider = (props) => {
       cartData[itemId] = quantity;
     }
     setCartItems(cartData);
+
+    if (user) {
+      try {
+        const token = await getToken();
+
+        await axios.post(
+          "/api/cart/update",
+          { cartData },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+
+        toast.success("Cart Updated");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    }
   };
 
   const getCartCount = () => {
