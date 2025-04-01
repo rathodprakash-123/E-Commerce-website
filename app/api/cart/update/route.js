a5b1d722b72510f6d8bc4ef7e0c89eaba0fda6e4
elyse502 <elyseniyibizi502@gmail.com>
2025-04-01 10:01:51 +0200

    Create API to save the Cart Data in the MongoDB Database (Update the Cart Data)

---
+++ app/api/cart/update/route.js
@@ -0,0 +1,21 @@
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+
+    const { cartData } = await request.json();
+
+    await connectDB();
+    const user = await User.findById(userId);
+
+    user.cartItems = cartData;
+    user.save();
+
+    NextResponse.json({ success: true });
+  } catch (error) {
+    NextResponse.json({ success: false, message: error.message });
+  }
+}
